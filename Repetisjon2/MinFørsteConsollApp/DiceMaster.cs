10b4f969dcc3d1903647bd2a2afc48cabbf2c927
marie <marie@getacademy.no>
2025-05-16 13:38:33 +0200

    chkchk

---
+++ Repetisjon2/MinFørsteConsollApp/DiceMaster.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinFørsteConsollApp
+{
+    internal class DiceMaster
+    {
+        Random random = new Random();
+        int numberOfThrownNumberOne = 0;
+        int numberOfThrownNumberTwos = 0;
+        int numberOfThrows = 10;
+
+        public void CompleteDiceThrows()
+        {
+            for (int i = 0; i < numberOfThrows; i++)
+            {
+                ThrowDiceAndIncStats();
+            }
+        }
+        public void SetNumberOfThrows(int newNumberOfThrows)
+        {
+            numberOfThrows = newNumberOfThrows;
+        }
+        public void ShowAllStatistics()
+        {
+            Console.WriteLine($"You threw {numberOfThrownNumberOne} 1`s");
+            Console.WriteLine($"You threw {numberOfThrownNumberTwos} 2`s");
+        }
+
+        void ThrowDiceAndIncStats()
+        {
+            var diceThrow = GetRandomDice();
+            if (diceThrow == 1)
+            {
+                numberOfThrownNumberOne++;
+            }
+            else if (diceThrow == 2)
+            {
+                numberOfThrownNumberTwos++;
+            }
+            Console.WriteLine($"Dice shows: {diceThrow}");
+        }
+        int GetRandomDice()
+        {
+            int diceResult = random.Next(1, 7);
+            return diceResult;
+        }
+    }
+}
---
+++ Repetisjon2/MinFørsteConsollApp/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+ *   
+Tema: Klasser/object
+Pause til 13.00
+
+OPPGAVE: 
+Dersom vi ønsker å velge spesifikt hvilket tall vi skal få statistikk for?
+*/
+
+using MinFørsteConsollApp;
+DiceMaster diceMaster = new DiceMaster();
+
+MainMenu();
+void MainMenu()
+{
+    Console.WriteLine("Welcome to the DICE THROWER! We will now throw the dice!");
+    Console.WriteLine("1. Choose number of dice throws (10 if nothing is specified)");
+    Console.WriteLine("2. Show All statistics");
+    Console.WriteLine("3. Show Specific statistic");
+
+    var menuChoice = Console.ReadLine();
+
+    switch (menuChoice)
+    {
+        case "1":
+            ChooseNumberOfDiceThrows();
+            diceMaster.CompleteDiceThrows();
+        break;
+        case "2":
+            diceMaster.CompleteDiceThrows();
+            diceMaster.ShowAllStatistics();
+            break;
+    }
+}
+
+void ChooseNumberOfDiceThrows()//Parameter => når vi ønsker å sende data INN til en funksjon
+{
+    Console.WriteLine("What number of throws?");
+    var numberOfThrows = int.Parse(Console.ReadLine());
+    diceMaster.SetNumberOfThrows(numberOfThrows);
+    //return numberOfThrows; -> RETURVERDI NÅR VI ØNSKER Å SENDE DATA UT AV FUNKSJON
+}
+
+
+
+
